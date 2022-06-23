5c4611b41a48c1c95238c20ed9f8e4f5fc65c146
sarahgibs <sgibson@broadinstitute.org>
2022-06-23 15:15:00 -0400

    update defect dojo location

--- scan.py
+++ scan.py
@@ -211,7 +211,7 @@ def testScan(proxy, script, env, project):
 
     reportFile = pullReport(zap, context, "https://" + site, site)
     export_reports.codedx_upload(project,reportFile)
-    export_reports.defectdojo_upload(216, reportFile, os.getenv("DOJO_PASS"), os.getenv("DOJO_USER"),"https://defectdojo.dsp-appsec-dev.broadinstitute.org")
+    export_reports.defectdojo_upload(216, reportFile, os.getenv("DOJO_PASS"), os.getenv("DOJO_USER"),"http://defectdojo.defectdojo.svc.cluster.local")
 
 
 
