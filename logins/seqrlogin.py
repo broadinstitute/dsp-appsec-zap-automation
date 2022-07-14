a44bb7638d61fa117d6d7f8f082a32a139110b68
sarahgibs <sgibson@broadinstitute.org>
2022-07-14 15:50:21 -0400

    returned chrome to headless

--- logins/seqrlogin.py
+++ logins/seqrlogin.py
@@ -16,9 +16,9 @@ def set_chrome_options(proxy) -> None:
     """
     chrome_options = Options()
     #When using this in a container, uncomment the lines below
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--proxy-server='+ proxy)
     chrome_options.add_argument('--allow-insecure-localhost')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
