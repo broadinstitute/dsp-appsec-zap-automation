d86942a43bf690ec7c1bc8b9abd9d678bca5a15a
sarahgibs <sgibson@broadinstitute.org>
2022-07-18 14:06:28 -0400

    add chrome profile support

--- logins/seqrlogin.py
+++ logins/seqrlogin.py
@@ -25,6 +25,8 @@ def set_chrome_options(proxy) -> None:
     chrome_options.add_argument('--window-size=1300,9000')
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--lang=en")
+    chrome_options.add_argument('--user-data-dir=/app/tmp/profile/')
+    chrome_options.add_argument('--profile-directory=Profile 6')
     return chrome_options
 
 def login(proxy, env, site):
@@ -55,47 +57,51 @@ def login(proxy, env, site):
             driver.add_cookie({"name": "accepted_cookies", "value": "true"})
             time.sleep(5)
             driver.get(url)
-            expected_conditions.title_is("seqr: Dashboard")
-            driver.find_element(by=By.LINK_TEXT, value="Log in").click()
-            #next page. should be google login.
-            expected_conditions.title_is("Sign in - Google Accounts")
-            time.sleep(4)
-            time.sleep(1)
-            driver.find_element(by=By.ID, value="identifierId").send_keys(os.getenv("SEQR_USER"))
-            time.sleep(3)
-            driver.find_element(by=By.ID, value="identifierNext").click()
-            #what in the what is the double parens.
-            time.sleep(30)
+             #check if logged in
             try:
-                driver.findElement(By.xpath("//*[text()='This browser or app may not be secure']"))
+                driver.find_element(by=By.LINK_TEXT, value="Log out")
+            except:
+                expected_conditions.title_is("seqr: Dashboard")
+                driver.find_element(by=By.LINK_TEXT, value="Log in").click()
+                #next page. should be google login.
+                expected_conditions.title_is("Sign in - Google Accounts")
+                time.sleep(4)
+                time.sleep(1)
+                driver.find_element(by=By.ID, value="identifierId").send_keys(os.getenv("SEQR_USER"))
+                time.sleep(3)
+                driver.find_element(by=By.ID, value="identifierNext").click()
+                #what in the what is the double parens.
+                time.sleep(30)
+                try:
+                    driver.findElement(By.xpath("//*[text()='This browser or app may not be secure']"))
+                    
+                except:
+                    logging.info("SEQR user was not flagged as using an insecure browser")
+                else:
+                    logging.error("Google flagged the browser as insecure, scan will not continue.")
+                    break
+                try:
+                    driver.find_element(by=By.ID, value="password")                
+                except:
+                    logging.error("Google is presenting a captcha, and scanning cannot continue")
+                    break
+                else:
+                    logging.info("Login will continue to password form. Google is not presenting a captcha")
+                #     #domstring=driver.execute_script("var xmlString = new XMLSerializer().serializeToString( document ); return xmlString;")
+                #     #print(domstring)
+
+                driver.implicitly_wait(3)
+                expected_conditions.presence_of_element_located((By.NAME, "password"))
+                driver.implicitly_wait(6)
+                driver.find_element(by=By.NAME, value="password").send_keys(os.getenv("SEQR_PASS"))
+                driver.implicitly_wait(3)
+                driver.find_element(by=By.ID, value="passwordNext").click()
                 
-            except:
-                logging.info("SEQR user was not flagged as using an insecure browser")
-            else:
-                logging.error("Google flagged the browser as insecure, scan will not continue.")
-                break
-            try:
-                driver.find_element(by=By.ID, value="password")                
-            except:
-                logging.error("Google is presenting a captcha, and scanning cannot continue")
-                break
-            else:
-                logging.info("Login will continue to password form. Google is not presenting a captcha")
-            #     #domstring=driver.execute_script("var xmlString = new XMLSerializer().serializeToString( document ); return xmlString;")
-            #     #print(domstring)
+                expected_conditions.title_contains("seqr")
+                expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"Summary Data"))
+                driver.find_element(by=By.LINK_TEXT, value="Summary Data").click()
 
-            driver.implicitly_wait(3)
-            expected_conditions.presence_of_element_located((By.NAME, "password"))
-            driver.implicitly_wait(6)
-            driver.find_element(by=By.NAME, value="password").send_keys(os.getenv("SEQR_PASS"))
-            driver.implicitly_wait(3)
-            driver.find_element(by=By.ID, value="passwordNext").click()
-            
-            expected_conditions.title_contains("seqr")
-            expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"Summary Data"))
-            driver.find_element(by=By.LINK_TEXT, value="Summary Data").click()
-
-            time.sleep(20)
+                time.sleep(20)
 
 
         except Exception as err:
