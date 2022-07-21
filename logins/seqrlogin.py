8ee2a70027e0cac2c587b705291e87225ca306be
sarahgibs <sgibson@broadinstitute.org>
2022-07-21 14:08:08 -0400

    add more checking to seqr login

--- logins/seqrlogin.py
+++ logins/seqrlogin.py
@@ -66,29 +66,33 @@ def login(proxy, env, site):
                 #next page. should be google login.
                 expected_conditions.title_is("Sign in - Google Accounts")
                 time.sleep(4)
-                time.sleep(1)
-                driver.find_element(by=By.ID, value="identifierId").send_keys(os.getenv("SEQR_USER"))
-                time.sleep(3)
-                driver.find_element(by=By.ID, value="identifierNext").click()
-                #what in the what is the double parens.
-                time.sleep(30)
                 try:
-                    driver.findElement(By.xpath("//*[text()='This browser or app may not be secure']"))
-                    
+                    driver.find_element(by=By.XPATH,value="//*[text()='Choose an account']")
+                    driver.find_element(by=By.XPATH,value="//*[text("+os.getenv("SEQR_USER")+")='']").click()
                 except:
-                    logging.info("SEQR user was not flagged as using an insecure browser")
-                else:
-                    logging.error("Google flagged the browser as insecure, scan will not continue.")
-                    break
-                try:
-                    driver.find_element(by=By.ID, value="password")                
-                except:
-                    logging.error("Google is presenting a captcha, and scanning cannot continue")
-                    break
-                else:
-                    logging.info("Login will continue to password form. Google is not presenting a captcha")
-                #     #domstring=driver.execute_script("var xmlString = new XMLSerializer().serializeToString( document ); return xmlString;")
-                #     #print(domstring)
+                    time.sleep(1)
+                    driver.find_element(by=By.ID, value="identifierId").send_keys(os.getenv("SEQR_USER"))
+                    time.sleep(3)
+                    driver.find_element(by=By.ID, value="identifierNext").click()
+                    #what in the what is the double parens.
+                    time.sleep(30)
+                    try:
+                        driver.findElement(By.xpath("//*[text()='This browser or app may not be secure']"))
+                        
+                    except:
+                        logging.info("SEQR user was not flagged as using an insecure browser")
+                    else:
+                        logging.error("Google flagged the browser as insecure, scan will not continue.")
+                        break
+                    try:
+                        driver.find_element(by=By.ID, value="password")                
+                    except:
+                        logging.error("Google is presenting a captcha, and scanning cannot continue")
+                        break
+                    else:
+                        logging.info("Login will continue to password form. Google is not presenting a captcha")
+                    #     #domstring=driver.execute_script("var xmlString = new XMLSerializer().serializeToString( document ); return xmlString;")
+                    #     #print(domstring)
 
                 driver.implicitly_wait(3)
                 expected_conditions.presence_of_element_located((By.NAME, "password"))
@@ -97,11 +101,11 @@ def login(proxy, env, site):
                 driver.implicitly_wait(3)
                 driver.find_element(by=By.ID, value="passwordNext").click()
                 
-                expected_conditions.title_contains("seqr")
-                expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"Summary Data"))
-                driver.find_element(by=By.LINK_TEXT, value="Summary Data").click()
+            expected_conditions.title_contains("seqr")
+            expected_conditions.presence_of_all_elements_located((By.LINK_TEXT,"Summary Data"))
+            driver.find_element(by=By.LINK_TEXT, value="Summary Data").click()
 
-                time.sleep(20)
+            time.sleep(20)
 
 
         except Exception as err:
