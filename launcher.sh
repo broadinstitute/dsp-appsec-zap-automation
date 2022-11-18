b10a2df045cb27ebe81919ed7b21bf9e3d83ca54
sarahgibs <sgibson@broadinstitute.org>
2022-11-18 11:44:12 -0500

    attempt

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-/zap/zap.sh -host 127.0.0.1 -port 8081 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.disablekey=true -daemon &
+/zap/zap.sh -Xmx10g -host 127.0.0.1 -port 8081 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.disablekey=true -daemon &
 python3 scan.py
 exit
